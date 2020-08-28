73cf800b7cbecfc55edc4a8e26a7caa47d727575
SolKul <46105598+SolKul@users.noreply.github.com>
2020-08-28 07:04:09 +0000

    same name function test

---
+++ exercise06/samples/sample.h
@@ -0,0 +1,7 @@
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+void func();
+void func_dummy();
+
+#endif //SAMPLE_H
---
+++ exercise06/samples/sample_1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "sample.h"
+
+void func(){
+  std::cout << "sample_1呼び出し" << std::endl;
+}
